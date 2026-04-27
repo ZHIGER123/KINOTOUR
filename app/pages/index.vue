a7bfe14c422b367dd90e05dbf32db7089f818489
Mansur  Mukhametzhanov <mansurlubitgusya@gmail.com>
2026-04-27 10:23:45 +0500

    bootstrap добавить

---
+++ app/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Welcome to the Home Page</h1>
+    <button class="btn btn-primary">Click me</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layouts: 'custom',
+})
+</script>
+
+<style>
+
+</style>
